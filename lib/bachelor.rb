36c34cdcd2fa02acd109f3967edf91c692137d6e
Sarah Evans <sarah.g.evans42@gmail.com>
2017-05-18 21:01:59 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,39 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find { |contestant_data| contestant_data["status"] == "Winner"}["name"].split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = ""
+  data.keys.each { |season| data[season].each { |contestant_data|
+    if contestant_data["occupation"] == occupation
+      contestant_name = contestant_data["name"]
+    end
+    }
+  }
+  contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.keys.each { |season| data[season].each { |contestant_data|
+    if contestant_data["hometown"] == hometown
+      count += 1
+    end
+    }
+  }
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.keys.each { |season|
+    data[season].each { |contestant_data| return contestant_data["occupation"] if contestant_data["hometown"] == hometown}
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0.0
+  data[season].each { |contestant_data| age += contestant_data["age"].to_i}
+  (age / data[season].size).round
 end
